09e647161eec06bced0285db7559ca8904a55838
c1f3r <cifer@yandex.ru>
2014-11-14 18:48:55 +0200

    ticket:1 created test which shouldn't pass

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
-class SomeTests(TestCase):
-    def test_math(self):
-        assert(2+2==5)
+
+
+class IndexTest(TestCase):
+    def test_index_page(self):
+        client = Client()
+        response = client.get(u'/')
+        self.assertEqual(response.status_code, 200)
