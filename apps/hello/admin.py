bd2bf93bf913240dd367ced0b15e82ff89307b05
c1f3r <cifer@yandex.ru>
2014-11-18 15:03:42 +0200

    ticket:1 created admin model to store info about me

--- apps/hello/admin.py
+++ apps/hello/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from apps.hello.models import AboutUser
 # Register your models here.
+
+
+class AboutUserAdmin(admin.ModelAdmin):
+    model = AboutUser
+    list_display = ('username', 'first_name', 'last_name', 'birth_date', 'email')
+
+
+admin.site.register(AboutUser, AboutUserAdmin)
