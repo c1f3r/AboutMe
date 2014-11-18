b0f366de76c5c975d1062c8ae8bfbdd6933a9555
c1f3r <cifer@yandex.ru>
2014-11-18 15:22:59 +0200

    ticket:1 added view, index template, edited base.html to use bootstrap, edited project urls.py and added app urls.py

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-
-admin.autodiscover()
+from views import ViewInfoAboutMe
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('apps.hello.urls'))
+    url(r'^$', ViewInfoAboutMe.as_view(), name='index')
 )
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from models import AboutUser
 
 # Create your views here.
+
+
+class ViewInfoAboutMe(DetailView):
+    """CBV for viewing info about me"""
+
+    model = AboutUser
+
+    def get_object(self, queryset=None):
+        return AboutUser.objects.get(username=u"cifer")
+
+    template_name = u'hello/index.html'
+    context_object_name = u'about_me'
