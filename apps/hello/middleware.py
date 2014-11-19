9fa156a9b6219b92b59b57b554026d5d28d567bf
c1f3r <cifer@yandex.ru>
2014-11-19 12:39:40 +0200

    ticket:3 created middleware and added it to settings

---
+++ apps/hello/middleware.py
@@ -0,0 +1,16 @@
+from apps.hello.models import HttpRequestLog
+
+
+class HttpRequestLogMiddleware:
+    """
+    middleware which saves each HttpRequest to DB
+    """
+
+    def process_request(self, request):
+        request_log = HttpRequestLog()
+        request_log.host = request.get_host()
+        request_log.path = request.get_full_path()
+        request_log.method = request.method
+        if request.user.is_authenticated():
+            request_log.user = request.user
+        request_log.save()
