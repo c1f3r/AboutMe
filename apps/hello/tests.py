63a6ccee8de0d26d631efed3d00ecd8c8a523908
c1f3r <cifer@yandex.ru>
2014-11-19 12:28:55 +0200

    ticket:3 added test which shouldn't passs

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.test import TestCase, Client
+from django.test import TestCase
 
 # Create your tests here.
 
@@ -7,14 +7,16 @@ from django.test import TestCase, Client
 class IndexTest(TestCase):
     fixtures = [u'initial_data.json']
 
-
-    def setUp(self):
-        self.client = Client()
-
     def test_index_page_exists(self):
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse(u'index'))
         self.assertEqual(response.status_code, 200)
 
     def test_index_page_contains_first_name(self):
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse(u'index'))
         self.assertContains(response, u'Artem')
+
+
+class TestHttpRequests(TestCase):
+    def test_requests_page_exists(self):
+        response = self.client.get(reverse(u'requests'))
+        self.assertEqual(response.status_code, 200)
