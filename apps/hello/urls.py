ce0bece78ed9c4b699d380f8703d92f7f39cd263
c1f3r <cifer@yandex.ru>
2014-11-19 13:00:50 +0200

    ticket:3 added south migrations, created template for requests page, edited index template and urls

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,10 +1,13 @@
-from django.conf.urls import patterns, include, url
-from views import ViewInfoAboutMe
+from django.conf.urls import patterns, url
+
+from views import ViewInfoAboutMe, HttpRequestList
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', ViewInfoAboutMe.as_view(), name='index')
+    url(r'^$', ViewInfoAboutMe.as_view(), name='index'),
+    url(r'^requests/$', HttpRequestList.as_view(), name='requests'),
 )
