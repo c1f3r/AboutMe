4748b359c0dea36299ee613e65f68806e2cc9850
c1f3r <cifer@yandex.ru>
2014-11-20 13:50:36 +0200

    ticket:5 added login/logout to index page

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -10,5 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^', include('apps.hello.urls'))
 )
