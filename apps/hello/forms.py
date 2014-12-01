d34046a135fd6c75bcff9f1288f9414f1cf5bb4a
c1f3r <cifer@yandex.ru>
2014-12-01 15:02:59 +0200

    ticket:6 removed annoying autocomplete in birh date input

--- apps/hello/forms.py
+++ apps/hello/forms.py
@@ -13,4 +13,5 @@ class EditInfoForm(forms.ModelForm):
     birth_date = forms.DateField(widget=DatePickerWidget(params="dateFormat: 'yy-mm-dd', changeYear: true, "
                                                                 "changeMonth: true, defaultDate: '-28y', "
                                                                 "yearRange: 'c-80:c+28'",
-                                                         attrs={'class': 'datepicker'}))
+                                                         attrs={'class': 'datepicker',
+                                                                'autocomplete': 'off'}))
