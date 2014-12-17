d1dd0484f000f85558ba6e0bea6ecf2667d029f3
c1f3r <cifer@yandex.ru>
2014-12-17 19:47:06 +0200

    ticket 9: made dat file write to uploads dir

--- list_models.sh
+++ list_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 _now=$(date +"%Y_%m_%d")
-_dir="assets"
-_file="$_dir/$_now.dat.css"
+_dir="uploads"
+_file="$_dir/$_now.dat"
 chmod 777 $_dir
 echo "Starting write to $_file"
 python manage.py list_models 2>> $_file
