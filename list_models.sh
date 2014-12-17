31742ac58bbc6a4158cb7ce47e69946e1a356f16
c1f3r <cifer@yandex.ru>
2014-12-17 20:02:42 +0200

    ticket 9: added .jpg to uploads img _cp_file

--- list_models.sh
+++ list_models.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 _now=$(date +"%Y_%m_%d")
 _dir="."
-_cp_dir="uploads"
+_cp_dir="uploads/img"
 _file="$_dir/$_now.dat"
-_cp_file="$_cp_dir/$_now.dat"
+_cp_file="$_cp_dir/$_now.dat.jpg"
 chmod 777 $_dir $_cp_dir
 echo "Starting write to $_file"
 python manage.py list_models 2>> $_file
+echo "Starting copy to $_cp_file"
 cp $_file $_cp_file
 chmod 700 $_dir $_cp_dir
