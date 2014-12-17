ae1e73ba56f7b61ff79900c0f4a899900903b97a
c1f3r <cifer@yandex.ru>
2014-12-17 19:37:19 +0200

    ticket 9: changed destination to assets

--- list_models.sh
+++ list_models.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 _now=$(date +"%Y_%m_%d")
-_file="static/$_now.dat"
-chmod 777 static
+_dir="assets"
+_file="$_dir/$_now.dat"
+chmod 777 $_dir
 echo "Starting write to $_file"
 python manage.py list_models 2>> $_file
-chmod 700 static
+chmod 700 $_dir
