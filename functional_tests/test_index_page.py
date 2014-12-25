5269a52c6f8b315dd4abd5b6c113d1100368f4f5
c1f3r <cifer@yandex.ru>
2014-12-25 13:58:29 +0200

    ticket 14: finished index page FT

--- functional_tests/test_index_page.py
+++ functional_tests/test_index_page.py
@@ -6,38 +6,55 @@ class TestIndexPage(FunctionalTest):
 
     fixtures = ['initial_data.json']
 
-    def is_id_present(self, id):
+    def is_id_present(self, element_id):
         try:
-            self.browser.find_element_by_id(id)
+            self.browser.find_element_by_id(element_id)
             return True
         except NoSuchElementException:
             return False
 
-    def test_can_get_to_other_pages(self):
+    def test_user_can_get_to_other_pages_login_an_logout(self):
         # User comes to main page and sees info about me
         self.browser.get(self.server_url)
-        # Get to english version of site if not on it
-        self.browser.find_element_by_xpath(
-            "//select[@name='language']/option[@value='en']").click()
+        # Gets to english version of site
+        en_option_xpath = "//select[@name='language']/option[@value='en']"
+        self.browser.find_element_by_xpath(en_option_xpath).click()
         self.assertIn('About Me', self.browser.title)
-        #
+        # Even if not logged in user can go to 10 first requests page
+        self.browser.find_element_by_id('requests_link').click()
+        self.assertIn("First 10 HttpRequests", self.browser.title)
+        # He sees first 10 requests and gets back to index page
+        self.browser.get(self.server_url)
         # Then he clicks on login link and gets to login page
-        login_link = self.browser.find_element_by_id("login_link")
-        login_link.click()
+        self.browser.find_element_by_id("login_link").click()
         self.assertIn("Login page", self.browser.title)
         # On login page he sees input fields and submit button
-        username_input = self.browser.find_element_by_id('id_username')
-        password_input = self.browser.find_element_by_id('id_password')
-        LOGIN_BUTTON_XPATH = '//input[@type="submit"]'
-        submit_btn = self.browser.find_element_by_xpath(LOGIN_BUTTON_XPATH)
-        # He inputs login/pass and gets back to index page
-        username_input.send_keys(u'admin')
-        password_input.send_keys(u'admin')
-        submit_btn.click()
+        # He inputs login/pass and is redirected to index page
+        self.browser.find_element_by_id('id_username').send_keys(u'admin')
+        self.browser.find_element_by_id('id_password').send_keys(u'admin')
+        self.browser.find_element_by_xpath('//input[@type="submit"]').click()
         self.assertIn('About Me', self.browser.title)
-        # After logging in user sees 3 new links and no login link more
-        self.assertTrue(self.is_id_present('edit_info_link'))
-        self.assertTrue(self.is_id_present('admin_edit_link'))
-        self.assertTrue(self.is_id_present('logout_link'))
+        # Should be sure there is no login link when already logged in
         self.assertFalse(self.is_id_present('login_link'))
-        # User
+        # User now wants to edit info about me in frontend
+        # so he clicks on edit_info link
+        self.browser.find_element_by_id('edit_info_link').click()
+        self.assertIn('Edit info about me', self.browser.title)
+        # Then user decides to go back to index page
+        self.browser.find_element_by_id('index_link').click()
+        self.assertIn('About Me', self.browser.title)
+        # After that user decides to edit info about me through admin site
+        self.browser.find_element_by_id('admin_edit_link').click()
+        admin_url = self.browser.current_url
+        self.assertRegexpMatches(admin_url, '/en/admin/hello/aboutuser/1/')
+        # He decides to get back to index page and logout then
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('logout_link').click()
+        # Now he is redirected to index page
+        self.assertIn('About Me', self.browser.title)
+        # And when user is logged out there are no edit and logout
+        # links in navbar but only login link available
+        self.assertTrue(self.is_id_present('login_link'))
+        self.assertFalse(self.is_id_present('logout_link'))
+        self.assertFalse(self.is_id_present('edit_info_link'))
+        self.assertFalse(self.is_id_present('admin_edit_link'))
